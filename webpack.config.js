e8f450c4a8a123630ebe8cc3d529892ab59deea9
csenn <csennabaum@gmail.com>
2015-07-22 23:19:24 -0700

    fixed tabs

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 
 /*
-	TODO: Scss Loader - move to css file
-	http://www.jonathan-petitcolas.com/2015/05/15/howto-setup-webpack-on-es6-react-application-with-sass.html
+TODO: Scss Loader - move to css file
+http://www.jonathan-petitcolas.com/2015/05/15/howto-setup-webpack-on-es6-react-application-with-sass.html
 */
 
 module.exports = {
 
-	entry: "./src/index.js",
+    entry: "./src/index.js",
 
-	output: {
-		path: __dirname + '/dist',
-		filename: "bundle.js"
-	},
+    output: {
+        path: __dirname + '/dist',
+        filename: "bundle.js"
+    },
 
-	module: {
+    module: {
 
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel',
-				query: {
-					optional: ['es7.decorators']
-				}
-			},
+        loaders: [
             {
-            	test: /\.scss$/,
-            	loaders: ['style', 'css', 'sass']
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    optional: ['es7.decorators']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
             }
-		]
-	}
+        ]
+    }
 };
